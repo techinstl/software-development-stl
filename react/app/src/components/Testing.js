4f3187d5d590f5332a779b379a250a0a96a2bef8
Mark Fang <mark.fang.mo@gmail.com>
2022-05-01 12:43:01 -0500

    changes

--- react/app/src/components/Testing.js
+++ react/app/src/components/Testing.js
@@ -1,26 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { useParams, useNavigate } from "react-router-dom";
 const Testing = (props) => {
-  const [quote, setQuote] = useState(null);
-
-  useEffect(() => {
-    axios.get("https://api.quotable.io/random").then((response) => {
-      console.log(response.data);
-      setQuote(response.data);
-    });
-  }, []);
+  const navigate = useNavigate();
+  const { message } = useParams();
 
   return (
     <div className="flex flex-col space-y-4 max-w-lg">
-      {quote ? (
-        <>
-          <h1 className="text-xl">{quote.author}</h1>
-          <p className="text-md">{quote.content}</p>
-        </>
-      ) : (
-        "fetching..."
-      )}
+      {message}
     </div>
   );
 };
