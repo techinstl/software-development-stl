2deb4a70c77e4372cd7188d20589374fc4c3b18a
Mark Fang <mark.fang.mo@gmail.com>
2022-05-01 12:20:42 -0500

    changes

--- react/app/src/components/Testing.js
+++ react/app/src/components/Testing.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const Testing = (props) => {
-
   const [quote, setQuote] = useState(null);
 
   useEffect(() => {
-    axios.get("https://api.quotable.io/random").then(response => {
+    axios.get("https://api.quotable.io/random").then((response) => {
       console.log(response.data);
       setQuote(response.data);
-    })
+    });
   }, []);
 
   return (
-    <div className="flex flex-col space-y-4 ">
-      <h1 className="text-xl">{quote.author}</h1>
-      <p className="text-md">{quote.content}</p>
+    <div className="flex flex-col space-y-4 max-w-lg">
+      {quote ? (
+        <>
+          <h1 className="text-xl">{quote.author}</h1>
+          <p className="text-md">{quote.content}</p>
+        </>
+      ) : (
+        "fetching..."
+      )}
     </div>
   );
 };
---
+++ react/app/src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Navbar from "../components/Navbar";
+import Testing from "../components/Testing";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center text-white bg-blue-400 bg-light-900">
+      <Navbar />
+      <div
+        id="content"
+        className="flex flex-col items-center space-y-24 mt-10 p-10"
+      >
+        <div className="flex flex-col items-center space-y-6">
+          <h1 className="text-6xl">
+            Hi! I'm{" "}
+            <span className="animate-pulse text-8xl italic bg-blue-200 p-2 rounded-xl text-black">
+              Mark Fang.
+            </span>
+          </h1>
+          <div id="divider" className="p-1 bg-black w-full rounded-xl"></div>
+        </div>
+        <About />
+        <Contact />
+        <Testing />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
